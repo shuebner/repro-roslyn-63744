a37c67f0becfef6c51224afff7b3ab19a49e0499
Sven Hübner <shuebner@qti.qualcomm.com>
2022-11-10 05:35:43 +0100

    init

---
+++ SyntaxHighlightingRepro/Repro63744.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SyntaxHighlightingRepro
+{
+    public class Repro63744
+    {
+        public static void Do()
+        {
+            Regex.Match("foo", @".*Setups\.Minimal\.(?<FileName>.*)");
+            new Regex("(.+)");
+            new Regex("(.*)");
+        }
+    }
+}
